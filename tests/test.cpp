44dc58b8e44ea158c0d75cf6cbc768d57977db5a
Strltz <145230912+Strltz@users.noreply.github.com>
2024-06-25 22:58:16 +0300

    Update test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -9,6 +9,21 @@ int b = 2;
 EXPECT_EQ(a, b - 1);
 }
 
+class MockClass : public A {
+public:
+  A::A() {
+	  mock->for_support();
+  }
+  MOCK_METHOD0(for_support, void());
+};
+
+TEST (tests, test_2) {
+  EXPECT_CALL(*mock, for_support())
+        .Times(1);
+  A* obj1 = A::getInstance();
+}
+
+
 TEST (tests, test_3) {
   A* obj1 = A::getInstance();
   EXPECT_EQ(obj1->getInstance(), nullptr);
