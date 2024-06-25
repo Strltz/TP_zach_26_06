4c8d7f3eec1fc2cfaf0e7128a9004657f09d64c4
Strltz <145230912+Strltz@users.noreply.github.com>
2024-06-25 22:20:28 +0300

    Create test.cpp

---
+++ tests/test.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "../class_support.cpp"
+
+TEST (tests, test_1) {
+int a = 1;
+int b = 2;
+EXPECT_EQ(a, b - 1);
+}
+
+TEST (tests, test_3) {
+  A* obj1 = A::getInstance();
+  //A* obj2 = A::getInstance();
+  EXPECT_EQ(obj1->getInstance(), nullptr);
+}
